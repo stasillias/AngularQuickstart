2400161c4e346c1cce85b893c8e818396ea45c86
Stanislav Nykyforuk <Stanislav_Nykyforuk@epam.com>
2017-08-04 23:03:30 +0300

    refactored heroes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
-import {AppRoutingModule} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports: [
