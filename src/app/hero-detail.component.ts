ade2535c77a44d1c0d6666c1f9816e751484fff4
Stanislav Nykyforuk <Stanislav_Nykyforuk@epam.com>
2017-08-04 23:22:11 +0300

    added styles

--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit }  from '@angular/core';
+import { Component, OnInit }  from '@angular/core';
 import { ActivatedRoute, ParamMap }  from '@angular/router';
 import { Location }  from '@angular/common';
 
@@ -9,10 +9,11 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'hero-detail',
-  templateUrl: './hero-detail.component.html'
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
-  @Input() hero: Hero;
+  hero: Hero;
 
   constructor(
     private heroService: HeroService,
